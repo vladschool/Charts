558afee65a19dab5a94f9d4730b66ebd57554342
Vlad Leshc <askjdnaskdjnas@gmail.com>
2023-12-10 23:00:43 +0200

    fix KI & MZI block, fix STS & KPS responsive

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-const progress = document.querySelector(".progress");
+(() => {
+  const bar = document.querySelector(".dashboard-bar__bar");
+  const valueBar = 75;
 
-const percent1 = Math.round((38 * 75) / 100);
+  bar.animate([{ transform: `rotate(${45 + valueBar * 1.8}deg)` }], {
+    duration: 3000,
+    fill: "forwards",
+    easing: `steps(${valueBar}, end)`,
+  });
 
-for (let index = 0; index < 38; index++) {
-  const className = index < percent1 ? "line" : "";
-  progress.innerHTML += `<i style="--i: ${index};" class="${className}"></i>`;
-}
+  const barLines = document.querySelector(".dashboard-bar__lines");
+  const valueBarLines = 60;
+
+  barLines.animate([{ transform: `rotate(${45 + valueBarLines * 1.8}deg)` }], {
+    duration: 3000,
+    fill: "forwards",
+    easing: `steps(${valueBarLines}, end)`,
+  });
+})();
